7448ac21b783679fb8927902239d52dc6015fd84
Yu Wei Chang <d60414@gmail.com>
2020-12-06 14:03:29 +0800

    add zh_TW language resource

---
+++ resources/lang/zh_TW/pagination.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; 上一頁',
+    'next' => '下一頁 &raquo;',
+    'Showing' => '顯示第',
+    'to' => '至',
+    'of' => '共',
+    'results' => '個結果',
+
+];
